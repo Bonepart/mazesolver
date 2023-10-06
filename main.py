30dabb5c8716aa9bbdd57d2d7d897b30552455a1
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-05 23:36:23 -0500

    add draw_move() method to Cell class

--- main.py
+++ main.py
@@ -50,10 +50,10 @@ class Cell:
         self.__top_wall = Line(point1, Point(point2.x, point1.y))
         self.has_bottom_wall = True
         self.__bottom_wall = Line(Point(point1.x, point2.y), point2)
-        self.__x1 = point1.x
-        self.__y1 = point1.y
-        self.__x2 = point2.x
-        self.__y2 = point2.y
+        self.x1 = point1.x
+        self.y1 = point1.y
+        self.x2 = point2.x
+        self.y2 = point2.y
         self.__win = window
 
     def draw(self):
@@ -66,10 +66,30 @@ class Cell:
         if self.has_bottom_wall:
             self.__win.draw_line(self.__bottom_wall, "purple")
 
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            fill_color = "gray"
+        else:
+            fill_color = "red"
+        point1 = Point(((self.x2 - self.x1) // 2) + self.x1, ((self.y2 - self.y1) // 2) + self.y1)
+        point2 = Point(((to_cell.x2 - to_cell.x1) // 2) + to_cell.x1, ((to_cell.y2 - to_cell.y1) // 2) + to_cell.y1)
+        line = Line(point1, point2)
+        print(f"x1: {line.x1} y1: {line.y1}")
+        print(f"x2: {line.x2} y2: {line.y2}")
+        self.__win.draw_line(line, fill_color)
+
 def main():
     win = Window(800,600)
-    cell1 = Cell(Point(10,10), Point(100,100), win)
+
+    #test code
+    cell1 = Cell(Point(10,10), Point(110,110), win)
+    cell1.has_right_wall = False
     cell1.draw()
+    cell2 = Cell(Point(110,10), Point(210,110), win)
+    cell2.has_left_wall = False
+    cell2.draw()
+    cell1.draw_move(cell2)
+
     win.wait_for_close()
 
 main()
