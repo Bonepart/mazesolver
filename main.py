b1010c36e92ea9b3c124199a8113eb7147789c27
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-05 23:13:39 -0500

    add Cell class

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter import Tk, BOTH, Canvas
 class Window:
     def __init__(self, height, width) -> None:
         self.__root = Tk()
-        self.__root.title = "Maze Solver"
+        self.__root.title("Maze Solver")
         self.canvas = Canvas(self.__root, height=height, width=width)
         self.canvas.pack()
         self.is_running = False
@@ -41,12 +41,35 @@ class Line:
         canvas.pack()
 
 class Cell:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, point1, point2, window) -> None:
+        self.has_left_wall = True
+        self.__left_wall = Line(point1, Point(point1.x, point2.y))
+        self.has_right_wall = True
+        self.__right_wall = Line(Point(point2.x, point1.y), point2)
+        self.has_top_wall = True
+        self.__top_wall = Line(point1, Point(point2.x, point1.y))
+        self.has_bottom_wall = True
+        self.__bottom_wall = Line(Point(point1.x, point2.y), point2)
+        self.__x1 = point1.x
+        self.__y1 = point1.y
+        self.__x2 = point2.x
+        self.__y2 = point2.y
+        self.__win = window
+
+    def draw(self):
+        if self.has_left_wall:
+            self.__win.draw_line(self.__left_wall, "purple")
+        if self.has_right_wall:
+            self.__win.draw_line(self.__right_wall, "purple")
+        if self.has_top_wall:
+            self.__win.draw_line(self.__top_wall, "purple")
+        if self.has_bottom_wall:
+            self.__win.draw_line(self.__bottom_wall, "purple")
 
 def main():
     win = Window(800,600)
-    
+    cell1 = Cell(Point(10,10), Point(100,100), win)
+    cell1.draw()
     win.wait_for_close()
 
 main()
