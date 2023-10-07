43fbabf5bedb9f38469efdca5223d03ce4af5c6a
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-07 09:14:37 -0500

    change to use new Window color data members

--- cell.py
+++ cell.py
@@ -1,4 +1,4 @@
-from graphics import Window, Line, Point
+from graphics import Window, Line
 
 class Cell:
     def __init__(self, window) -> None:
@@ -21,46 +21,45 @@ class Cell:
             self.y2 = y2
         if self.x1 is None or self.y1 is None or self.x2 is None or self.y2 is None:
             raise Exception("Do not have coordinates for Cell Location")
-        point1 = Point(self.x1, self.y1)
-        point2 = Point(self.x2, self.y2)
-        fill_color = None
 
         if self.has_left_wall:
-            fill_color = "black"
+            left_wall = Line(self.x1, self.y1, self.x1, self.y2)
+            self.__win.draw_line(left_wall, self.__win.draw_color)
         else:
-            fill_color = "white"
-        left_wall = Line(point1, Point(point1.x, point2.y))
-        self.__win.draw_line(left_wall, fill_color)
+            left_wall = Line(self.x1, self.y1 + 1, self.x1, self.y2 - 1)
+            self.__win.draw_line(left_wall, self.__win.background_color)
 
         if self.has_right_wall:
-            fill_color = "black"
+            right_wall = Line(self.x2, self.y1, self.x2, self.y2)
+            self.__win.draw_line(right_wall, self.__win.draw_color)
         else:
-            fill_color = "white"
-        right_wall = Line(Point(point2.x, point1.y), point2)
-        self.__win.draw_line(right_wall, fill_color)
+            right_wall = Line(self.x2, self.y1 + 1, self.x2, self.y2 - 1)
+            self.__win.draw_line(right_wall, self.__win.background_color)
 
         if self.has_top_wall:
-            fill_color = "black"
+            top_wall = Line(self.x1, self.y1, self.x2, self.y1)
+            self.__win.draw_line(top_wall, self.__win.draw_color)
         else:
-            fill_color = "white"
-        top_wall = Line(point1, Point(point2.x, point1.y))
-        self.__win.draw_line(top_wall, fill_color)
+            top_wall = Line(self.x1 + 1, self.y1, self.x2 - 1, self.y1)
+            self.__win.draw_line(top_wall, self.__win.background_color)
 
         if self.has_bottom_wall:
-            fill_color = "black"
+            bottom_wall = Line(self.x1, self.y2, self.x2, self.y2)
+            self.__win.draw_line(bottom_wall, self.__win.draw_color)
         else:
-            fill_color = "white"
-        bottom_wall = Line(Point(point1.x, point2.y), point2)
-        self.__win.draw_line(bottom_wall, fill_color)
+            bottom_wall = Line(self.x1 + 1, self.y2, self.x2 - 1, self.y2)
+            self.__win.draw_line(bottom_wall, self.__win.background_color)
 
     def draw_move(self, to_cell, undo=False):
         if undo:
             fill_color = "gray"
         else:
             fill_color = "red"
-        point1 = Point(((self.x2 - self.x1) // 2) + self.x1, ((self.y2 - self.y1) // 2) + self.y1)
-        point2 = Point(((to_cell.x2 - to_cell.x1) // 2) + to_cell.x1, ((to_cell.y2 - to_cell.y1) // 2) + to_cell.y1)
-        line = Line(point1, point2)
-        print(f"x1: {line.x1} y1: {line.y1}")
-        print(f"x2: {line.x2} y2: {line.y2}")
+        x1 = (self.x2 - self.x1) // 2 + self.x1
+        y1 = (self.y2 - self.y1) // 2 + self.y1
+        x2 = (to_cell.x2 - to_cell.x1) // 2 + to_cell.x1
+        y2 = (to_cell.y2 - to_cell.y1) // 2 + to_cell.y1
+        line = Line(x1, y1, x2, y2)
+        #print(f"x1: {line.x1} y1: {line.y1}")
+        #print(f"x2: {line.x2} y2: {line.y2}")
         self.__win.draw_line(line, fill_color)
