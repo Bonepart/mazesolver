7963121ad9696fa49b4f769bd79aa0f68cbd1d43
Bonepart <28548170+Bonepart@users.noreply.github.com>
2023-10-11 22:12:31 -0500

    update main.py

--- main.py
+++ main.py
@@ -8,11 +8,7 @@ def main():
 
     win = Window(20 + num_rows * cell_size, 20 + num_cols * cell_size)
     m1 = Maze(10, 10, num_rows, num_cols, cell_size, cell_size, win)
-    if m1.solve():
-        print("Solved Maze")
-    else:
-        print("Solve Failed")
-
+    m1.solve()
     win.wait_for_close()
 
 main()
